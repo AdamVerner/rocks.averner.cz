c93f9bcb273f556826500f058e9f3071a1d38f74
Viola Kosíková <violakosikova@gmail.com>
2023-05-30 14:25:30 +0200

    no unused imports, extra div elements

--- app/src/components/RockMap.tsx
+++ app/src/components/RockMap.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 import database from "../data/database.json";
 
@@ -13,32 +12,31 @@ import {
   ZoomControl,
   KeyboardControl,
 } from "react-mapycz";
+
 const RockMap = () => {
   return (
-    <div>
-      <Map zoom={10} center={{ lat: 50.52, lng: 14.9 }} height={"100vh"}>
-        <KeyboardControl />
-        <ZoomControl />
-        <MouseControl zoom={true} pan={true} wheel={true} />
-        <CompassControl right={10} top={50} />
-        <SyncControl />
-        <MarkerLayer>
-          {database.entries.map((e) => {
-            let head = `<p>${e.grade} - <strong>${e.name} - (${e.rock})</strong></p>`;
-            let body = `<p><a href="${e.link}" target="_blank">Source link</a></p><p>${e.description}</p>`;
-            return (
-              <Marker
-                coords={{ lat: e.lat, lng: e.long }}
-                card={{
-                  header: head,
-                  body: body,
-                }}
-              />
-            );
-          })}
-        </MarkerLayer>
-      </Map>
-    </div>
+    <Map zoom={10} center={{ lat: 50.52, lng: 14.9 }} height={"100vh"}>
+      <KeyboardControl />
+      <ZoomControl />
+      <MouseControl zoom={true} pan={true} wheel={true} />
+      <CompassControl right={10} top={50} />
+      <SyncControl />
+      <MarkerLayer>
+        {database.entries.map((e) => {
+          const head = `<p>${e.grade} - <strong>${e.name} - (${e.rock})</strong></p>`;
+          const body = `<p><a href="${e.link}" target="_blank">Source link</a></p><p>${e.description}</p>`;
+          return (
+            <Marker
+              coords={{ lat: e.lat, lng: e.long }}
+              card={{
+                header: head,
+                body: body,
+              }}
+            />
+          );
+        })}
+      </MarkerLayer>
+    </Map>
   );
 };
 
